caee02f0f98db18e1d8b61d58f10ba58f9ef0699
Phạm Minh Khang <khangpmk2004@gmail.com>
2025-09-15 21:35:02 +0700

    merge to deploy (#7)
    * Feature/home screen (#5)
    
    * init tailwin
    
    * coding HomeScreen
    
    * update HomeScreen - hero/landing page
    
    * Feature/home screen (#6)
    
    * init tailwin
    
    * coding HomeScreen
    
    * update HomeScreen - hero/landing page
    
    * add Footer
    
    * add Sharing screen
    
    * update Header hover animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import HomeScreenProvider from "./context/HomeScreenProvider";
 import HomeScreen from "./page/HomeScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PortfolioScreen from "./page/PortfolioScreen";
+import Sharing from "./page/Sharing";
 
 function App() {
   return (
-    <HomeScreenProvider>
-      <Router>
+    <Router>
+      <HomeScreenProvider>
         <Routes>
           <Route path="/">
             <Route index element={<HomeScreen />} />
           </Route>
+          <Route path="/portfolio" element={<PortfolioScreen />} />
+          <Route path="/sharing" element={<Sharing />} />
         </Routes>
-      </Router>
-    </HomeScreenProvider>
+      </HomeScreenProvider>
+    </Router>
   );
 }
 
